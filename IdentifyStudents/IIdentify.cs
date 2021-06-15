adbb34c0712c887e67b0a25f45707b2ab4093c56
InterNos <kirilrknu@gmail.com>
2021-06-15 12:14:39 +0300

    Add IIdentify (interface)

---
+++ IdentifyStudents/IIdentify.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentifyStudents
+{
+    interface IIdentify
+    {
+        double DecodePhoto(double photoCode);
+
+        long IdentifyStudents();
+    }
+}
