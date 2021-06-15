55ec3b043624622f869c8ec5d0b47f2b1315d33d
InterNos <kirilrknu@gmail.com>
2021-06-15 12:19:12 +0300

    Add Identify that implement IIdentify

---
+++ IdentifyStudents/Identify.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentifyStudents
+{
+    class Identify : IIdentify
+    {
+        /// <summary>
+        /// Get Photo code and decoded it
+        /// </summary>
+        /// <param name="photoCode"></param>
+        /// <returns>Photo that can be processed by func</returns>
+        public double DecodePhoto(double photoCode) 
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Work with decoded students
+        /// </summary>
+        /// <returns>Count of Students</returns>
+        public long IdentifyStudents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
